42c59d0b527733c42f6172beaa078fda8c690fdb
ralva <ralva@DESKTOP-NRPEC9O>
2017-11-15 21:05:04 +0100

    ADTS 1st commit - RAS

---
+++ src/adts/Test.java
@@ -0,0 +1,17 @@
+package adts;
+
+/**
+ * @author Ricardo Alvarez Sancho
+ * @version 1.0.0
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
